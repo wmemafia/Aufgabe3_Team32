f15be1f67017d5e5f3416931fe97a148aa1001b3
Michael <schneider.mic@gmx.de>
2016-11-24 12:33:25 +0100

    initial commit with given template

---
+++ app.js
@@ -0,0 +1,34 @@
+// DO NOT CHANGE!
+//init app with express, util, body-parser, csv2json
+var express = require('express');
+var app = express();
+var sys = require('util');
+var path = require('path');
+var bodyParser = require('body-parser');
+var Converter = require("csvtojson").Converter;
+
+//register body-parser to handle json from res / req
+app.use( bodyParser.json() );
+
+//register public dir to serve static files (html, css, js)
+app.use( express.static( path.join(__dirname, "public") ) );
+
+// END DO NOT CHANGE!
+
+
+/**************************************************************************
+****************************** csv2json *********************************
+**************************************************************************/
+
+/**************************************************************************
+********************** handle HTTP METHODS ***********************
+**************************************************************************/
+
+
+// DO NOT CHANGE!
+// bind server to port
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
