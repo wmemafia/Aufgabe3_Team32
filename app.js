d6e5ad51c3edb5f5d294d82c9bb98c7ff0a212d9
Michael <schneider.mic@gmx.de>
2016-12-05 13:59:47 +0100

    continued implementing get responses

--- app.js
+++ app.js
@@ -35,11 +35,17 @@ converter.fromFile("./world_data.csv", function(err, result) {
 * GET-Methods
 */
 
+
+/**
+* get all countries wirh properties
+*/
 app.get('/items', function(req, res) {
     res.send(JSON.stringify(data, null, 4));
 });
 
-
+/**
+* get country with given id and properties
+*/
 app.get('/items/:id', function(req, res) {
     var id = req.params.id;
     var country = data.find(function(element) {
@@ -51,6 +57,49 @@ app.get('/items/:id', function(req, res) {
         res.status(404).send('No such id {' + id + '} in database.');
     } 
 });
+
+/**
+* get countries in defined range with properties
+*/
+app.get('/items/:id1/:id2', function(req, res) {
+    var id1 = req.params.id1 <= req.params.id2 ? req.params.id1 : req.params.id2;
+    var id2 = req.params.id1 >= req.params.id2 ? req.params.id1 : req.params.id2;
+    var result = [];
+    for (i = id1; i <= id2; i++){
+        if(country = data.find(function(element) {
+            return element.id == i;
+        })) {
+            result.push(country);
+        }
+    }
+    if (result.length == 0) {
+        res.status(400).send('Range not possible.');
+    } else {
+        res.send(JSON.stringify(result, null, 4));
+    }
+});
+
+/**
+* returns all the properties of a data object
+*/
+app.get('/properties', function(req, res) {
+    if(data.length > 0) {
+        res.send(JSON.stringify(Object.keys(data[0])));
+    } else {
+        res.send('No data available');
+    }
+});
+
+app.get('/properties/:num', function(req, res) {
+    var num = req.params.num;
+    if(data.length > 0) {
+        // #TODO: check if num in bounds of array (NaN ??)
+        var keys = Object.keys(data[0]);
+        res.send(keys[num]);
+    } else {
+        res.send('No data available');
+    }
+})
 
 
 /**
